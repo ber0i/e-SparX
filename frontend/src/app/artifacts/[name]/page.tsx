62919c5dc7a5e1fe962eb9e5cc6524d03409297f
ber0i <annikakristina.schneider@tum.de>
2024-09-19 09:46:36 +0200

    modify artifact view depending on artifact type

--- frontend/src/app/artifacts/[name]/page.tsx
+++ frontend/src/app/artifacts/[name]/page.tsx
@@ -99,12 +99,6 @@ export default function ArtifactDetailPage({ params }: { params: { name: string 
           )}
         </p>
 
-        <p className="key">Number of Rows:</p>
-        <p className="value">{artifact.num_rows ? artifact.num_rows : "not available"}</p>
-
-        <p className="key">Number of Columns:</p>
-        <p className="value">{artifact.num_columns ? artifact.num_columns : "not available"}</p>
-
         <p className="key">Pipelines:</p>
         <div className="value">
           {pipelines.length > 0 ? (
@@ -121,49 +115,61 @@ export default function ArtifactDetailPage({ params }: { params: { name: string 
           )}
         </div>
       </div>
-      
-      {artifact.data_schema && artifact.data_schema.length > 0 ? (
-        <div className="overflow-x-auto">
-          <h2>Dataset Schema</h2>
-          <div className="artifact-info">
-            <p className="key">Index:</p>
-            <p className="value">{artifact.index_name} (data type: {artifact.index_dtype})</p>
-          </div>
-          <table className="min-w-full leading-normal border">
-            <thead>
-              <tr>
-                <th className="px-5 py-3 border-b-2 border-brand-smoke bg-accent text-left text-xs font-semibold text-contrast uppercase tracking-wider ">
-                  Column Name
-                </th>
-                <th className="px-5 py-3 border-b-2 border-brand-smoke bg-accent text-left text-xs font-semibold text-contrast uppercase tracking-wider ">
-                  Data Type
-                </th>
-                <th className="px-5 py-3 border-b-2 border-brand-smoke bg-accent text-left text-xs font-semibold text-contrast uppercase tracking-wider ">
-                  Required
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {artifact.data_schema.map((colSpec, index) => (
-                <tr key={index}>
-                  <td className="px-5 py-5 border-b border-brand-smoke bg-canvas text-sm dark:bg-accent dark:border-primary">
-                    <div className="text-contrast whitespace-no-wrap">
-                      {colSpec.name}
-                    </div>
-                  </td>
-                  <td className="px-5 py-5 border-b border-brand-smoke bg-canvas text-sm dark:bg-accent dark:border-primary">
-                    {colSpec.type}
-                  </td>
-                  <td className="px-5 py-5 border-b border-brand-smoke bg-canvas text-sm dark:bg-accent dark:border-primary">
-                    {colSpec.required ? "Yes" : "No"}
-                  </td>
+
+      {artifact.artifact_type === "dataset" && (
+        <>
+        {artifact.data_schema && artifact.data_schema.length > 0 ? (
+          <div className="overflow-x-auto">
+            <h2>Dataset Schema</h2>
+            <div className="artifact-info">
+
+              <p className="key">Number of Rows:</p>
+              <p className="value">{artifact.num_rows ? artifact.num_rows : "not available"}</p>
+
+              <p className="key">Number of Columns:</p>
+              <p className="value">{artifact.num_columns ? artifact.num_columns : "not available"}</p>
+
+              <p className="key">Index:</p>
+              <p className="value">{artifact.index_name} (data type: {artifact.index_dtype})</p>
+
+            </div>
+            <table className="min-w-full leading-normal border">
+              <thead>
+                <tr>
+                  <th className="px-5 py-3 border-b-2 border-brand-smoke bg-accent text-left text-xs font-semibold text-contrast uppercase tracking-wider ">
+                    Column Name
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-brand-smoke bg-accent text-left text-xs font-semibold text-contrast uppercase tracking-wider ">
+                    Data Type
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-brand-smoke bg-accent text-left text-xs font-semibold text-contrast uppercase tracking-wider ">
+                    Required
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      ) : (
-        <p>No schema available for this artifact.</p>
+              </thead>
+              <tbody>
+                {artifact.data_schema.map((colSpec, index) => (
+                  <tr key={index}>
+                    <td className="px-5 py-5 border-b border-brand-smoke bg-canvas text-sm dark:bg-accent dark:border-primary">
+                      <div className="text-contrast whitespace-no-wrap">
+                        {colSpec.name}
+                      </div>
+                    </td>
+                    <td className="px-5 py-5 border-b border-brand-smoke bg-canvas text-sm dark:bg-accent dark:border-primary">
+                      {colSpec.type}
+                    </td>
+                    <td className="px-5 py-5 border-b border-brand-smoke bg-canvas text-sm dark:bg-accent dark:border-primary">
+                      {colSpec.required ? "Yes" : "No"}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <p>No schema available for this artifact.</p>
+        )}
+        </>
       )}
     </div>
   );
