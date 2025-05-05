19acf2df7e92f0850199806a581494e26cd156b7
ber0i <a.k.schneider@tum.de>
2025-05-05 15:39:47 +0200

    small fixes

--- python_api/esparx/_user_config.py
+++ python_api/esparx/_user_config.py
@@ -11,7 +11,7 @@ user_config: "UserConfigModel" = None
 @dataclass
 class UserConfigModel:
     user_id: str
-    api_base: str = "http://10.152.14.197:8080"
+    api_base: str = "http://localhost:8080"
 
 
 def get_user_config_path():
